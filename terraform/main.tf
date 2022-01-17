6720aa0eeac76b48bf4747efae57f943884650e4
NickGlebanov <n.n.glebanov@yandex.ru>
2022-01-17 23:40:56 +0300

    server group with balancer

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,13 +17,18 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
 # Указываем, что мы хотим разворачивать окружение в AWS
 provider "aws" {
   region = "eu-central-1"
-}
-
-resource "aws_eip" "server" {
-  instance = aws_instance.web.id
 }
 
 
@@ -36,49 +41,116 @@ variable "domain" {
   default = "diplomproj.ru"
 }
 
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-  user_data              = <<-EOF
+resource "aws_eip" "server" {
+  vpc = true
+}
+
+
+#конфигурация серверов входящих в автоскейлинг группу
+resource "aws_launch_configuration" "example" {
+  image_id        = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.alb.id]
+
+  user_data = <<-EOF
 #!/bin/bash
 echo "Hello, World" > index.html
 nohup busybox httpd -f -p 8080 &
 EOF
+}
 
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  min_size             = 2
+  max_size             = 2
+  min_elb_capacity     = 2
+  health_check_type    = "ELB"
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+  target_group_arns    = [aws_lb_target_group.asg.arn]
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+  # Требуется при использовании группы автомасштабирования
+  # в конфигурации запуска.
+  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+# балансер
+
+resource "aws_lb" "main" {
+  name               = "terraform-asg-example"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+  internal           = false
+  load_balancer_type = "application"
 
 }
 
-resource "cloudflare_record" "www" {
-  zone_id = "cc965f05e57b7751af5448a0d15ac29f"
-  name    = "www"
-  value   = aws_eip.server.public_ip
-  type    = "A"
-  proxied = true
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404"
+      status_code  = 404
+    }
+  }
 }
 
-resource "cloudflare_record" "root" {
-  zone_id = "cc965f05e57b7751af5448a0d15ac29f"
-  name    = var.domain
-  value   = aws_eip.server.public_ip
-  type    = "A"
-  proxied = true
-}
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
 
-resource "cloudflare_record" "test" {
-  zone_id = "cc965f05e57b7751af5448a0d15ac29f"
-  name    = "test"
-  value   = aws_eip.server.public_ip
-  type    = "A"
-  proxied = true
-}
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  # Разрешаем все
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+}
+
+
+output "alb_dns_name" {
+  value       = aws_lb.main.dns_name
+  description = "The domain name of the load balancer"
 }
